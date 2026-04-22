7711afa62a03f443a6dacfea3a7a0af62698f7b3
muffindohoah <uwufunkytown@gmail.com>
2026-04-22 06:12:49 -0500

    delivered jude the tools inshallah

--- PlayerControllers/Abilities/EvilGun/evil_gun_ability.gd
+++ PlayerControllers/Abilities/EvilGun/evil_gun_ability.gd
@@ -44,16 +44,9 @@ func _process(delta: float) -> void:
 	if !is_multiplayer_authority(): return
 	if !currently_active: return
 	
-	crosshair_002.visible = visible
-	global_transform = merc.camera.global_transform
-	
 	# Don't allow shooting or reloading while already reloading
 	if animation_player.is_playing() and animation_player.current_animation == "reload": 
 		return
-		
-	if Input.is_action_just_pressed("reload") and ammo < max_ammo:
-		reload()
-
 	# 2. Handle Single vs Auto fire inputs
 	var trigger_pulled: bool = false
 	if is_auto:
@@ -75,40 +68,7 @@ func reload():
 	label.text = str(ammo) + "/" + str(max_ammo)
 
 func shoot():
-	if ammo <= 0:
-		# Optional: Play a "click" sound here for empty ammo
-		return
-	
-	# Consume 1 ammo per trigger pull (even if it's a shotgun firing 8 pellets)
-	ammo = clamp(ammo - 1, 0, max_ammo)
-	
-	
-	# Restart animation and start the cooldown timer
-	animation_player.stop() 
-	animation_player.play("fire")
-	fire_attack_speed.start()
-	label.text = str(ammo) + "/" + str(max_ammo)
-	# 4. Fire every raycast in the array (1 for Pistol, Many for Shotgun)
-	_do_raycasts()
-
-func _do_raycasts() -> void:
-	for rc in raycasts:
-		if not is_instance_valid(rc): continue
-		
-		# Force update so the raycast is perfectly aligned with the camera this frame
-		rc.force_raycast_update()
-
-		if rc.is_colliding():
-			var person_hit = rc.get_collider()
-			if person_hit != self and person_hit != null and person_hit is Merc:
-				person_hit.take_damage.rpc_id(int(person_hit.name), damage)
-				
-			# Spawn tracer at hit point
-			tracer_effect._create_tracer_effect.rpc(tracer_effect.global_position, rc.get_collision_point())
-		else:
-			# Spawn tracer fading off into the distance if they missed
-			var miss_point = rc.global_transform * rc.target_position
-			tracer_effect._create_tracer_effect.rpc(tracer_effect.global_position, miss_point)
+	pass
 
 func equip():
 	show()
